added req dump and added fixed rotation around d/2

algorithm/src-design/fake_till_you_make.py
@@ -59,7 +59,7 @@ def on_click(event: MouseEvent):
                     color="green", fontsize=8)
 
         if len(measurement_points) > 1:
-            dist = int(input("measured length between the 2 points (cm): ").strip())
+            dist = float(input("measured length between the 2 points (cm): ").strip())
             p1, p2 = measurement_points[-2], measurement_points[-1]
             mid = (p1 + p2) / 2                        # numpy midpoint
 
@@ -136,22 +136,18 @@ def robot_do_instructions(instructions: list[Instruction]):
         
 
 if __name__ == "__main__":
-    if (len(sys.argv) < 7): 
+    if (len(sys.argv) < 5): 
         #normalized locations are specified 0 to 1 relative to the shape of the image
         #initial heading is angle of the robot measured from +x in degrees CCW POS
-        raise "usage: file.py image_path output_path normalized_start_location_x normalized_start_location_y initial_heading_deg dump_mode"
+        raise "usage: file.py image_path output_path initial_heading_deg dump_mode"
     abs_outpath = os.path.abspath(sys.argv[2]);
-    dump_mode = int(sys.argv[6])
+    dump_mode = int(sys.argv[4])
     if (dump_mode):
         abs_impath = os.path.abspath(sys.argv[1]);
         
         img = cv.imread(abs_impath, cv.IMREAD_GRAYSCALE);
         img = cv.GaussianBlur(img, (0,0), 1.6);
         edges = cv.Canny(img, 150, 200);
-        
-        loc = (round(float(sys.argv[3]) * img.shape[1]) , round(float(sys.argv[4]) * img.shape[0]));
-        
-        clicked_points = np.vstack([clicked_points, loc]);
         
         fig, ax = plt.subplots();
         ax.imshow(edges, cmap="gray");
@@ -168,7 +164,7 @@ if __name__ == "__main__":
         
         # initial heading as a +x-CCW angle (deg) -> unit direction vector, since
         # point_array_to_instructions / calculate_signed_angle operate on vectors.
-        initial_theta = np.radians( float(sys.argv[5]));
+        initial_theta = np.radians( float(sys.argv[3]));
         initial_heading = np.array([np.cos(initial_theta), np.sin(initial_theta)]);
 
         instructions: list[Instruction] = point_array_to_instructions(clicked_points_real, initial_heading);